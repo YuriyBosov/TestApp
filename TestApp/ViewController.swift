4e1ad1e972417c1d18528866598fb0c025bd134f
Yurii Bosov <bosov.yu@gmail.com>
2020-11-28 14:07:58 +0200

    created project;

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Yurii Bosov on 28.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
